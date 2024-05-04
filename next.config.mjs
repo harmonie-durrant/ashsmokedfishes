f180370c116058c17ec937f419e3fe408342fe9b
Harmonie ✨ <harmoniedurrant@gmail.com>
2024-05-04 16:00:28 +0000

    Fix env

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const withPWA = withPWAInit({
 });
 
 export default withPWA({
+    env: {
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+      },
     images: {
         domains: ['firebasestorage.googleapis.com', 'i.imgur.com'],
     },
